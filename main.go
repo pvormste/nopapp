01e0fe15b70c085ede357b8eb0e27fe553d41ef1
Patric Vormstein <pvormstein@googlemail.com>
2026-03-31 13:20:19 +0200

    add comment

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	// following line serves no real purpuse, just a reminder what credential to use
+	// Run app via: MY_SECRET_CREDENTIAL=secret_cred ./nopapp
 	os.Setenv("MY_SECRET_CREDENTIAL", "secret_cred")
 	fmt.Println(noplib.CallToNopLib())
 }
